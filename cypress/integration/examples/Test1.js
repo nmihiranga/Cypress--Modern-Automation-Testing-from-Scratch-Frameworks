e6598947e717f46c0b53a8cb74c8eb8a628a38b3
nmihiranga <mihiranganayana@gmail.com>
2025-03-12 13:59:20 +0530

    Add Cypress test cases for product selection and checkout process

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -5,19 +5,27 @@ describe('My First Test Suite', function() {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca');
 
+    cy.get('.products').as('productLocator');
+
     cy.get('.product:visible').should('have.length', 4);
     // Parent child chaining
-    cy.get('.products').find('.product').should('have.length', 4);
+    cy.get('@productLocator').find('.product').should('have.length', 4);
 
-    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click();
     // Same as above, but recommended to use above one
     // cy.get(':nth-child(3) > .product-action > button').click();
 
-    cy.get('.products').find('.product').each(($el, index, $list) => {
+    cy.get('@productLocator').find('.product').each(($el, index, $list) => {
       const textVeg = $el.find('h4.product-name').text();
       if(textVeg.includes('Cashews')) {
         cy.wrap($el).find('button').click();
       }
+    });
+
+    cy.get('.brand').should('have.text', 'GREENKART');
+
+    cy.get('.brand').then(function(logoElement) {
+      cy.log(logoElement.text());
     });
 
   });
---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,23 @@
+describe('My Second Test Suite', function() {
+
+  it('My First TestCase', function() {
+
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    cy.get('.search-keyword').type('ca');
+
+    cy.get('.products').as('productLocator');
+
+    cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+      const textVeg = $el.find('h4.product-name').text();
+      if(textVeg.includes('Cashews')) {
+        cy.wrap($el).find('button').click();
+      }
+    });
+
+    cy.get('.cart-icon > img').click();
+    cy.contains('PROCEED TO CHECKOUT').click();
+    cy.contains('Place Order').click();
+
+  });
+
+})
