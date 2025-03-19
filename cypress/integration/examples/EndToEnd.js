0e09068af35fc67f14ed76a1203cb8947a641604
nmihiranga <mihiranganayana@gmail.com>
2025-03-19 21:57:18 +0530

    Refactor end-to-end test to use fixture data for login and add validation for total amount before checkout

--- cypress/integration/examples/EndToEnd.js
+++ cypress/integration/examples/EndToEnd.js
@@ -1,12 +1,19 @@
 describe('End to End Test', () => {
 
-  it('My End to End Test case', () => {
-    const productName = 'Nokia Edge';
+  before(function () { // Use `function` instead of `() =>` when using Fixtures
+    cy.fixture('example').then((data) => {
+      this.data = data;
+    });
+  });
+
+  it('My End to End Test case', function () { // Use `function` instead of `() =>` when using Fixtures
+
+    const productName = this.data.productName;
 
     cy.visit('https://rahulshettyacademy.com/loginpagePractise/');
-    cy.get('#username').type('rahulshettyacademy');
+    cy.get('#username').type(this.data.username);
 
-    cy.get('#password').type('learning');
+    cy.get('#password').type(this.data.password);
     cy.contains('Sign In').click();
 
     cy.contains('Shop Name').should('be.visible');
@@ -27,6 +34,22 @@ describe('End to End Test', () => {
 
     cy.get('app-card').eq(0).contains('button', 'Add').click();
     cy.contains('a', 'Checkout').click();
+
+    let sum = 0;
+    cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+      const amount = $el.text();
+      let res = amount.split(' ')[1].trim();
+      sum = Number(sum) + Number(res);
+    }).then(() => {
+      expect(sum).to.be.lessThan(200000);
+    });
+
+    cy.contains('button', 'Checkout').click();
+    cy.get('#country').type('India');
+    cy.get('.suggestions > ul > li > a', { timeout: 10000 }).click();
+    //cy.get('.checkbox').click();
+    cy.contains('input', 'Purchase').click();
+    cy.get('.alert').should('contain', 'Success!');
 
   });
 
